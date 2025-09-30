2f291c9020aab0e27bddcbace702110d8f086be1
Robin <61976533+iRobin93@users.noreply.github.com>
2025-09-30 14:35:43 +0200

    commits

--- src/model/generalData.js
+++ src/model/generalData.js
@@ -1,2 +1,3 @@
 
-export const BASE_URL = 'http://localhost:8080/api';
+//export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'https://gamereview.calmwave-ef10540f.uksouth.azurecontainerapps.io/api';
